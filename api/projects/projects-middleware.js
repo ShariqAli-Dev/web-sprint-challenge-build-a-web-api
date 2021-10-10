f7708270ffe3f85df8a26ea80a6ea5d8e58653d9
ShariqAli-Dev <shariqali.dev@gmail.com>
2021-10-10 18:41:10 -0400

    added middleware validateProjectId

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,22 @@
-// add middlewares here related to projects
+const Projects = require('./projects-model');
+
+async function validateProjectId(req, res, next) {
+  try {
+    const projectMaybe = await Projects.get(req.params.id);
+
+    if (projectMaybe) {
+      req.project = projectMaybe;
+      next();
+    } else {
+      next(
+        res
+          .status(404)
+          .json({ message: `Project with id ${req.params.id} not found` })
+      );
+    }
+  } catch (err) {
+    next(res.status(500).json(err.message));
+  }
+}
+
+module.exports = { validateProjectId };
