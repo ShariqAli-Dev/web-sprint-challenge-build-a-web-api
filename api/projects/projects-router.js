9764c67c8f65a4092d507fc3f4d8079cb82fc0f7
ShariqAli-Dev <shariqali.dev@gmail.com>
2021-10-10 18:41:35 -0400

    completed get projects/:id/

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,6 +1,7 @@
 // Write your "projects" router here!
 const express = require('express');
 const Projects = require('./projects-model');
+const { validateProjectId } = require('./projects-middleware');
 
 const router = express.Router();
 
@@ -10,6 +11,10 @@ router.get('/', (req, res) => {
       res.status(201).json(projects);
     })
     .catch((err) => res.status(500).json({ message: 'Server Error' }));
+});
+
+router.get('/:id', validateProjectId, (req, res) => {
+  res.status(201).json(req.project);
 });
 
 module.exports = router;
