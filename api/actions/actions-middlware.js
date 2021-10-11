0f0969c60e9928a2de559d18ff61996ffac0dbee
ShariqAli-Dev <shariqali.dev@gmail.com>
2021-10-10 20:50:38 -0400

    middleware validateActionId, get actions/

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,21 @@
-// add middlewares here related to actions
+const Actions = require('./actions-model');
+
+async function validateActionId(req, res, next) {
+  try {
+    const maybeAction = await Actions.get(req.params.id);
+
+    if (maybeAction) {
+      req.action = maybeAction;
+      next();
+    } else {
+      next({
+        status: 404,
+        message: `Action with id ${req.params.id} not found`,
+      });
+    }
+  } catch (err) {
+    next({ status: 500, message: err });
+  }
+}
+
+module.exports = { validateActionId };
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const Actions = require('./actions-model');
+const { validateActionId } = require('./actions-middlware');
 
 const router = express.Router();
 
-module.exports = { router };
+router.get('/', (req, res) => {
+  Actions.get()
+    .then((actions) => {
+      if (actions.length === 0) {
+        res.status(201).json([]);
+      } else {
+        res.status(201).json(actions);
+      }
+    })
+    .catch((err) =>
+      res.status(500).json('Error grabbing actions from databse')
+    );
+});
+
+module.exports = router;
